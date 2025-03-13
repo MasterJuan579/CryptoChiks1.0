464f5f59536d030000667a02dbf1692a335c9820
MasterJuan579 <juanperezgutierrez579@gmail.com>
2025-03-13 04:53:37 -0600

    Game2 Terminado
    La primera leccion de la blockchain lista

---
+++ CryptoChiks2.0/Assets/Scripts/QuizLesson1/AnswerScript.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class AnswerScript : MonoBehaviour
+{
+    public bool isCorrect = false;
+    public QuizManager quizManager;
+    public void Answer(){
+        if(isCorrect){
+            Debug.Log("Correct Answer");
+            quizManager.correct();
+        }
+        else{
+            Debug.Log("Wrong Answer");
+            quizManager.wrong();
+        }
+    }
+}
